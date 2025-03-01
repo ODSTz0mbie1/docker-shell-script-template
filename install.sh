40789d1b001f66d13d0f17d45ea944c83ce12683
Seamus Smith <seamus@sfo.me.uk>
2025-03-01 23:26:22 +0000

    Auto remove container when script is finished, to prevent creating multiple containers.

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 docker build -t script1 . || { echo "Docker build failed"; exit 1; }
 
 # Define the alias string
-ALIAS="alias testscript='docker run -v .:/app/data script1'"
+ALIAS="alias testscript='docker run --rm -v .:/app/data script1'"
 
 # Check if the alias already exists in the current session
 if alias testscript >/dev/null 2>&1; then
