d26ce4aa74986d9f665d527eb620592ecbcfff3b
Seamus Smith <seamus@sfo.me.uk>
2025-03-02 21:47:32 +0000

    sperated code into functions

--- main.py
+++ main.py
@@ -4,21 +4,30 @@ import os
 import argparse
 import pandas as pd # type: ignore
 
-def main():
-    # argparse is a module for parsing command-line arguments.
-    # It allows you to define what arguments your program requires, and handles parsing those arguments from sys.argv.
-    parser = argparse.ArgumentParser(description="A script template with argument parsing.")
-    parser.add_argument('-d', '--directory', type=str, help='Directory to process', default=os.getcwd())
-    args = parser.parse_args()
-    current_directory = args.directory
-    print(os.listdir(current_directory))
+def processing(process_directory):
     df = pd.DataFrame()
     df['A'] = [1, 2, 3]
     df['B'] = [4, 5, 6]
     df['C'] = [7, 8, 9]
-    df.to_csv(os.path.join(current_directory, "output.csv"), index=False)
+    df.to_csv(os.path.join(process_directory, "output.csv"), index=False)
     print("Dataframe saved to output.csv")
-    print(os.listdir(current_directory))
+    return
+
+def main():
+    # argparse is a module for parsing command-line arguments.
+    # It allows you to define what arguments your program requires, and handles parsing those arguments from sys.argv.
+    parser = argparse.ArgumentParser(description="A script template with argument parsing.")
+
+    # Add arguments
+    parser.add_argument('-d', '--directory', type=str, help='Sets the directory to process', default=os.getcwd())
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+
+    process_directory = args.directory
+    processing(process_directory)
+    
 
 
 if __name__ == "__main__":
